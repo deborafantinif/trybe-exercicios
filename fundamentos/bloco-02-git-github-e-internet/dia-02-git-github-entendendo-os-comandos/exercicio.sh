754c3446acadf6f09136facc0f937ebcb75d5adb
deobrafantinif <deboraferfan@gmail.com>
2021-07-22 11:57:56 -0300

    resolução dos exercícios do modulo1 - bloco 2 - dia 2

---
+++ fundamentos/bloco-02-git-github-e-internet/dia-02-git-github-entendendo-os-comandos/exercicio.sh
@@ -0,0 +1,38 @@
+#1
+cd /home/debora/Documents/trybe/trybe-exercicios/fundamentos/bloco-02-git-github-e-internet/dia-02-git-github-entendendo-os-comandos
+
+#2
+git status
+
+#3
+git checkout -b trybe-skills-change
+
+#4
+cat >> trybe_skills.txt
+- HTML
+- CSS
+git add trybe_skills.txt
+git commit -m "Adiciona novas skills"
+git push -u origin trybe-skills-change
+
+#5
+#entrar no GitHub e realizar o Pull Request
+
+#6
+git checkout main
+
+#7
+git branch
+
+#8
+git checkout -b branch trybe-skills-updates
+
+#9 
+cat >> trybe_skills.txt
+git add trybe_skills.txt
+git commit -m "Adiciona nova skill"
+git push -u origin trybe-skills-updates
+
+#10
+#entrar no GitHub e fazer o merge com as correções necessárias
+
