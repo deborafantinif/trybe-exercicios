37c145df38312b8a0fab51f880a0205b70277a17
deobrafantinif <deboraferfan@gmail.com>
2021-07-28 18:57:35 -0300

    uso do switch case ao inves do if no exercicio 6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/exercicio-06.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-01-javascript-primeiros-passos/exercicio-06.js
@@ -1,24 +1,25 @@
 //source: https://www.soxadrez.com.br/conteudos/movimentos/
-let pecaXadrez = 'Peão'
+let pecaXadrez = 'BiSPo'
 
-if(pecaXadrez.toLowerCase() == 'rei') {
-    console.log('Pode mover-se em qualquer direção, porém apenas uma casa.')
-}
-else if(pecaXadrez.toLowerCase() == 'dama') {
-    console.log('Pode mover-se em qualquer direção (vertical, horizontal e diagonal), quantas casas quiser, desde que estejam livres.')
-}
-else if(pecaXadrez.toLowerCase() == 'torre') {
-    console.log('Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.')
-}
-else if(pecaXadrez.toLowerCase() == 'bispo') {
-    console.log('Move-se na diagonal, quantas casas quiser.')
-}
-else if(pecaXadrez.toLowerCase() == 'cavalo') {
-    console.log('Pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas, conhecida como L.')
-}
-else if(pecaXadrez.toLowerCase() == 'peão') {
-    console.log('Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas.')
-}
-else {
-    console.log('Valor inserido inválido!')
+switch(pecaXadrez.toLowerCase()) {
+    case 'rei':
+        console.log('Pode mover-se em qualquer direção, porém apenas uma casa.')
+        break;
+    case 'dama':
+        console.log('Pode mover-se em qualquer direção (vertical, horizontal e diagonal), quantas casas quiser, desde que estejam livres.')
+        break;
+    case 'torre':
+        console.log('Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser.')
+        break;
+    case 'bispo':
+        console.log('Move-se na diagonal, quantas casas quiser.')
+        break;
+    case 'cavalo':
+        console.log('Pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas, conhecida como L.')
+        break;
+    case 'peão':
+        console.log('Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas.')
+        break;
+    default:
+        console.log('Valor inserido inválido!')
 }
