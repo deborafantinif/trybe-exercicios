65ec05ee00d23c2b137e2c992e670ac92313bd87
deobrafantinif <deboraferfan@gmail.com>
2021-08-02 14:52:48 -0300

    resoluçao dos requisitos 5 do para fixar parte I - modulo 1 - bloco 4 - dia 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-04-javascript-objetos-e-funcoes/para-fixar-01.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-04-javascript-objetos-e-funcoes/para-fixar-01.js
@@ -29,3 +29,6 @@ console.table(player)
 //requisito 04 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.')
 
+
+//requisito 05
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de pata.')
