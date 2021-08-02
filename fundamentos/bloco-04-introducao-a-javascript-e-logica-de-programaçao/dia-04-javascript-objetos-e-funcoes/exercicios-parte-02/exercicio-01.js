94244be7f8778e689faf02a36588ee4055267a53
deobrafantinif <deboraferfan@gmail.com>
2021-08-02 19:10:27 -0300

    resoluçao do exercicio 1 - parte II - modulo 1 - bloco 4 - dia 4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-02/exercicio-01.js
@@ -0,0 +1,17 @@
+function vereficaPalindrome(palavra) {
+    //processo de inverter a palavra
+    let separaValores = palavra.split("")
+    let inverteValores = separaValores.reverse()
+    let juntaValores = inverteValores.join("")
+
+    //verificação do palíndromo
+    if(palavra === juntaValores) {
+        return true
+    } else {
+        return false
+    }
+
+
+}
+
+console.log(vereficaPalindrome('ovo'))
