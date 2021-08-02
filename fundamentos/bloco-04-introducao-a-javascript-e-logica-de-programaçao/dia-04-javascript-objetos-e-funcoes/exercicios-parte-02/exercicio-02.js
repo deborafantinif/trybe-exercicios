ff972f8f2eddf3727e40620af63e49f18a7ea22a
deobrafantinif <deboraferfan@gmail.com>
2021-08-02 19:19:52 -0300

    resoluçao do exercicio 2 - parte II - modulo 1 - bloco 4 - dia 4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programaçao/dia-04-javascript-objetos-e-funcoes/exercicios-parte-02/exercicio-02.js
@@ -0,0 +1,11 @@
+function maiorValor(array) {
+    for(let i = 0; i < array.length; i += 1) {
+        let maior;
+        if(array[i] > array[i + 1]) {
+            maior = i
+            return maior
+        }
+    }
+}
+
+console.log(maiorValor([2, 3, 6, 7, 10, 1]))
