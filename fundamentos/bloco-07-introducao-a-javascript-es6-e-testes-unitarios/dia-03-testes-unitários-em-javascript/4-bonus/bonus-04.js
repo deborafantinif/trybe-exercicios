e483ef1fd7235f2c56bb62003f188cff6ab1113e
deobrafantinif <deboraferfan@gmail.com>
2021-08-31 19:15:30 -0300

    resolucao bonus parte iv

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-testes-unitários-em-javascript/4-bonus/bonus-04.js
@@ -0,0 +1,17 @@
+const getLargestNumber = (array) => {
+  let largestNumber = array[0];
+  for (let index = 0; index <= array.length; index += 1) {
+      if (array[index] > largestNumber) {
+        largestNumber = [array[index]];
+      }
+  }
+  return largestNumber[0];
+}
+
+const assert = require('assert');
+
+const parameter = [45, 8, 2, 50, 1, 7, 99];
+const result = 99;
+const output = getLargestNumber(parameter);
+
+assert.deepStrictEqual(output, result);
