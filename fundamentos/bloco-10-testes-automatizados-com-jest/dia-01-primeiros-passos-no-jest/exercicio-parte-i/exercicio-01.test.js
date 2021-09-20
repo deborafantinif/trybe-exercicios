22b4ffec84841c8d24603984b430c438157b8a33
deobrafantinif <deboraferfan@gmail.com>
2021-09-20 19:08:29 -0300

    resolucao parte i - exercicio 1

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-01-primeiros-passos-no-jest/exercicio-parte-i/exercicio-01.test.js
@@ -0,0 +1,15 @@
+const sum = require('./exercicio-01.js');
+
+describe('Verigicação da função SUM', () => {
+  it('Teste se o retorno de sum(4, 5) é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('Teste se o retorno de sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => {sum(4, '5')}).toThrowError(new Error("parameters must be numbers"));
+  })
+})
