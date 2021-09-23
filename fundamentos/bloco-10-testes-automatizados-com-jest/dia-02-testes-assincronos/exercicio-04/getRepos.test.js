36d24c5b158740792686b652914bb0e42075b348
deobrafantinif <deboraferfan@gmail.com>
2021-09-23 00:53:05 -0300

    resolucao exercicio 05

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-testes-assincronos/exercicio-04/getRepos.test.js
@@ -0,0 +1,8 @@
+const getRepos = require('./getRepos');
+
+describe('Verifica a função getRepos', () => {
+  it('verifica se o repositório sd-01-week4-5-project-todo-list está contido no retorno de getRepos', async () => {
+    await expect(getRepos('https://api.github.com/orgs/tryber/repos'))
+      .resolves.toContain('sd-01-week4-5-project-todo-list')
+  });
+});
---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-testes-assincronos/exercicio-05.js/beforeAndAfter.test.js
@@ -0,0 +1,13 @@
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+
+test('', () => console.log('1 - test'));
+
+describe('Scoped / Nested block', () => {
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+
+  test('', () => console.log('2 - test'));
+});
+
+//Errei o segundo teste, pois não anotei que o before 1 e o after 1 também seriam executados.
